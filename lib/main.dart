870c245fe7ff944d1351164c5f6a8dacb809c57c
R-Z-7 <shazzkallan@gmail.com>
2022-10-05 19:28:42 +0530

    test2

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter SignUp ',
+      title: 'Flutter SignUp Demo ',
       home: MyHomePage(),
     );
   }
